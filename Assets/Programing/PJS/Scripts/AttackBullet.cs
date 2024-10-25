323e8f6fd327d18f4be590c043432ba4450fabdd
anMois <kew1672@naver.com>
2024-10-25 18:02:39 +0900

    [Feat] : 투사체 진행 과정
    캐릭터와 몬스터의 투사체 기능 구현
    누구한테서 나온 투사체이며 누가 피해를 입는지를 확인

--- Assets/Programing/PJS/Scripts/AttackBullet.cs
+++ Assets/Programing/PJS/Scripts/AttackBullet.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class AttackBullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum Parent { Player, Monster}   // 누구의 투사체인지 정하기
+    [SerializeField] Parent curPerant;
+    public Parent CurPerant { set { curPerant = value; } }  //get도 필요하면 사용자가 작성
+    [SerializeField] Rigidbody rigid;
+    [SerializeField] float speed;
+
+    private void Start()
     {
-        
+        rigid.velocity = Vector3.forward * speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        // 플레이어가 쏘고 몬스터가 맞은 경우
+        if(collision.transform.CompareTag("Monster") && curPerant == Parent.Player)
+        {
+            // 몬스터가 피격되는 기능 작업
+        }
+        // 몬스터가 쏘고 플레이어가 맞은 경우
+        else if(collision.transform.CompareTag("Player") && curPerant == Parent.Monster)
+        {
+            // 플레이어가 피격되는 기능 작업
+        }
+
+        // 벽에 맞은 경우
+        Destroy(gameObject);
     }
 }
