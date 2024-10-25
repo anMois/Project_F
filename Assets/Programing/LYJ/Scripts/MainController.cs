8a0700167961e2fe3da22bb887d5ac1ed0b9d3c6
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-10-25 20:07:36 +0900

    [Feat]: 메뉴얼 기능 추가
    메뉴얼(게임설명)을 보여주는 스크롤뷰 개발

--- Assets/Programing/LYJ/Scripts/MainController.cs
+++ Assets/Programing/LYJ/Scripts/MainController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,20 +8,22 @@ public class MainController : MonoBehaviour
     private GameObject mainCanvas;
     private GameObject bookCanvas;
     private GameObject explanationCanvas;
+    private GameObject manualCanvas;
+
 
     private void Start()
     {
         mainCanvas = GameObject.Find("Main Canvas");
         bookCanvas = GameObject.Find("Book Canvas");
-
         explanationCanvas = GameObject.Find("Explanation Canvas");
+        manualCanvas = GameObject.Find("Manual Canvas");
 
-        if (mainCanvas != null && bookCanvas != null && explanationCanvas != null)
+        if (mainCanvas != null && bookCanvas != null && explanationCanvas != null && manualCanvas != null)
         {
             mainCanvas.SetActive(true);
             bookCanvas.SetActive(false);
-
             explanationCanvas.SetActive(false);
+            manualCanvas.SetActive(false);
         }
     }
 
@@ -60,27 +60,33 @@ public class MainController : MonoBehaviour
 
     public void ClickBookButton()
     {
-        if (mainCanvas != null && bookCanvas != null)
-        {
-            mainCanvas.SetActive(false);
-            bookCanvas.SetActive(true);
-        }
+        mainCanvas.SetActive(false);
+        bookCanvas.SetActive(true);
     }
 
     public void BookExitButton()
     {
-        if (mainCanvas != null && bookCanvas != null)
-        {
-            mainCanvas.SetActive(true);
-            bookCanvas.SetActive(false);
-        }
+        mainCanvas.SetActive(true);
+        bookCanvas.SetActive(false);
+    }
+
+    public void ClickManualButton()
+    {
+        mainCanvas.SetActive(false);
+        manualCanvas.SetActive(true);
+    }
+
+    public void ManualExitButton()
+    {
+        mainCanvas.SetActive(true);
+        manualCanvas.SetActive(false);
     }
 
     public void ExitButton()
     {
         //유니티 에디터에서는 Application.Quit();를 사용할 수 없음 테스트 용도로 아래 코드 사용
         UnityEditor.EditorApplication.isPlaying = false;
-        
+
         //빌드해서 사용할 때는 아래 코드를 사용
         //Application.Quit();    
     }
