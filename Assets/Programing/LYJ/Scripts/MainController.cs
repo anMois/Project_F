d731a114d4b92be337b2e1ac63b1953ced599106
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-10-25 12:14:00 +0900

    [Feat]: 게임시작 및 도감 버튼 구현
    New game 버튼을 누르면 스테이지1로 씬 전환, Item Book 버튼을 누르면 로비 UI 비활성화 후 도감 UI 활성화

---
+++ Assets/Programing/LYJ/Scripts/MainController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainController : MonoBehaviour
+{
+    private GameObject mainCanvas;
+    private GameObject bookCanvas;
+
+    private void Start()
+    {
+        mainCanvas = GameObject.Find("Main Canvas");
+        bookCanvas = GameObject.Find("Book Canvas");
+
+        if (mainCanvas != null && bookCanvas != null)
+        {
+            mainCanvas.SetActive(true);
+            bookCanvas.SetActive(false);
+        }
+    }
+
+    public void Change_Stage1_Scene()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+
+    public void Click_Book_Button()
+    {
+        if (mainCanvas != null && bookCanvas != null)
+        {
+            mainCanvas.SetActive(false);
+            bookCanvas.SetActive(true);
+        }
+    }
+}
