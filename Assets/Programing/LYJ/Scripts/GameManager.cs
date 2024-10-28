0e69cd7d7a4989beaeebfd48af9869c2b5e6c5a4
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-10-28 18:35:33 +0900

    [Feat]: 제품 재구매 불가 기능 구현
    상점에서 한 번 구매한 제품은 다시 구매하지 못하도록 버튼 비활성화 (회색) 처리함

---
+++ Assets/Programing/LYJ/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    [SerializeField] public float curPrice = 10000f;
+    [SerializeField] TextMeshProUGUI priceText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        UpdatePriceText();
+    }
+
+    public bool PurchaseItem(float itemPrice)
+    {
+        if (curPrice >= itemPrice)
+        {
+            curPrice -= itemPrice;
+            UpdatePriceText();
+            return true;
+        }
+        return false;
+    }
+
+    private void UpdatePriceText()
+    {
+        if (priceText != null)
+        {
+            priceText.text = $"Possession Gold: {curPrice} G";
+        }
+    }
+}
--- Assets/Programing/LYJ/Scripts/StoreItemButton.cs
+++ Assets/Programing/LYJ/Scripts/StoreItemButton.cs
@@ -7,16 +7,25 @@ public class StoreItemButton : MonoBehaviour
 {
     public ItemData itemData;
     private StoreController storeController;
-    private Button button;
+    [SerializeField] Button button;
+    [SerializeField] Button itemBuyButton;
+
+    private bool isPurchased = false;
 
     private void Start()
     {
         storeController = FindObjectOfType<StoreController>();
         button = GetComponent<Button>();
+        itemBuyButton = transform.Find("Item Buy Button").GetComponent<Button>();
 
         if (button != null)
         {
             button.onClick.AddListener(OnButtonClick);
+        }
+
+        if (itemBuyButton != null)
+        {
+            itemBuyButton.onClick.AddListener(ItemBuyButtonClick);
         }
     }
 
@@ -26,5 +35,37 @@ public class StoreItemButton : MonoBehaviour
         {
             storeController.ShowExplanation(itemData.itemName, itemData.description, itemData.itemImage);
         }
+    }
+
+    public void ItemBuyButtonClick()
+    {
+        if (isPurchased) // 이미 구매한 경우
+        {
+            Debug.Log($"{itemData.itemName}은 이미 구매되었습니다.");
+            return; // 구매하지 않도록 조기 리턴
+        }
+
+        bool success = GameManager.Instance.PurchaseItem(itemData.price);
+        if (success)
+        {
+            Debug.Log($"{itemData.itemName}을(를) 구매했습니다.");
+            Debug.Log($"남은 돈: {GameManager.Instance.curPrice}");
+            isPurchased = true;
+            UpdateButtonState();
+        }
+        else
+        {
+            Debug.Log("돈이 부족하여 구매할 수 없습니다.");
+            Debug.Log($"남은 돈: {GameManager.Instance.curPrice}");
+        }
+    }
+
+    private void UpdateButtonState()
+    {
+        ColorBlock colors = itemBuyButton.colors;
+        colors.normalColor = Color.gray;
+        colors.disabledColor = Color.gray;
+        itemBuyButton.colors = colors;
+        itemBuyButton.interactable = false;
     }
 }
