d1e1ba3266b70725d053ea813db83b1051692e0c
KimSesame <kimbs082787@gmail.com>
2024-10-28 11:44:52 +0900

    [Feat]: 공격수단 Projectile 구현

---
+++ Assets/Programing/KHJ/Scripts/Projectile.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [Tooltip("Interactable layers")]
+    [SerializeField] LayerMask layerMask;
+    [Tooltip("Limit time since projectile launched")]
+    [SerializeField] float lifeTime;
+    [SerializeField] float speed;
+    [SerializeField] int dmg;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        IDamageable damageable = collision.gameObject.GetComponent<IDamageable>();
+
+        if (damageable != null)
+        {
+            damageable.TakeHit(dmg);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Launch projectile to target
+    /// </summary>
+    /// <param name="friendlyLayer">layer of shooter's friendly</param>
+    /// <param name="target">target to set projectile direction</param>
+    public void Launch(int friendlyLayer, Transform target)
+    {
+        // Ignore friendly layer
+        layerMask &= ~friendlyLayer;
+
+        // Fire
+        Vector3 direction = (transform.position - target.position).normalized;
+        rb.velocity = direction * speed;
+
+        Destroy(gameObject, lifeTime);
+    }
+}
