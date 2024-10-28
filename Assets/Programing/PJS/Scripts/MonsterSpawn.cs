cdda3160e02111f993d4ef778fecdb9a055cdf20
anMois <kew1672@naver.com>
2024-10-28 20:34:34 +0900

    [Feat] 몬스터 스폰 구현
    몬스터 배치 테이블이 가지고 몬스터 스폰 구현 중

---
+++ Assets/Programing/PJS/Scripts/MonsterSpawn.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawn : MonoBehaviour
+{
+    [SerializeField] List<Transform> spawnPoints;
+    [SerializeField] List<GameObject> spawnObjects;
+}
