31799c163b0e7c64effd342fe4d578d29d7d7ef6
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-10-29 19:08:02 +0900

    [Feat[: 시작 유물 스크립터블 오브젝트 구현
    게임 시작시 유물을 선택하게 되는데 그 유물을 스크립터블 오브젝트로 관리 구현함

---
+++ Assets/Programing/LYJ/Scripts/StartItemData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item_", menuName = "StartItem")]
+public class StartItemData : ScriptableObject
+{
+    public string itemName;
+    public string specialEffects;
+    public string specialEffectsDescription;
+    public Sprite itemImage;
+
+    public Sprite specialEffectsImage1;
+    public Sprite specialEffectsImage2;
+    public Sprite specialEffectsImage3;
+
+    public string specialEffectsFigure1;
+    public string specialEffectsFigure2;
+    public string specialEffectsFigure3;
+}
