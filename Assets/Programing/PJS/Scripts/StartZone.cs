9302677d77d59d113ea7fdc9a463fe902a549142
anMois <kew1672@naver.com>
2024-10-30 13:57:16 +0900

    [Feat] 플레이어 안전구역 구현
    플레이어가 안전구역에서 벗어나면 벗어났다는 이벤트 발생

---
+++ Assets/Programing/PJS/Scripts/StartZone.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StartZone : MonoBehaviour
+{
+    public UnityAction OnAreaOut;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.transform.CompareTag("Player"))
+        {
+            Debug.Log("³ª¿È");
+            OnAreaOut?.Invoke();
+            GetComponent<SphereCollider>().enabled = false;
+        }
+    }
+}
