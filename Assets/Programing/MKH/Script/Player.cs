30e3ee9e79a6ddb375b654e96c41e98cfe46ee4a
MoonKyoungHun <lsf8355@gmail.com>
2024-10-31 14:03:46 +0900

    [Feat]: 플레이어 상태 스크립트, 프리팹 생성
    플레이어 hp 및 피격 스크립트 추가
    
    플레이어 프리팹 생성

---
+++ Assets/Programing/MKH/Script/Player.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Player : MonoBehaviour, IDamageable
+{
+    public UnityAction OnAlarm;
+
+    [SerializeField] PlayerMover playerMover;
+
+
+    [Header("Attributes")]
+    [SerializeField] int maxHp;
+    [SerializeField] public int curHp;
+
+    private int friendlyLayer;
+
+    private void Awake()
+    {
+        playerMover = GetComponent<PlayerMover>();
+        friendlyLayer = LayerMask.GetMask("Player");
+
+        curHp = maxHp;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F1))
+        {
+            curHp--;
+
+            if (curHp <= 0)
+                return;
+        }
+
+    }
+
+    public void TakeHit(int dmg)
+    {
+        OnAlarm?.Invoke();
+
+        curHp -= dmg;
+        if (curHp <= 0)
+        {
+            playerMover.AnimaitorPlay();
+        }
+    }
+
+
+}
