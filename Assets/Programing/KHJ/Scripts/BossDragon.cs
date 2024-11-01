8a9b2ad54cd1877072140e9620f0a7e1437a1a56
KimSesame <kimbs082787@gmail.com>
2024-11-01 18:56:58 +0900

    [Feat]: 전방 지상 공격 시 플레이어 근처로 이동

--- Assets/Programing/KHJ/Scripts/BossDragon.cs
+++ Assets/Programing/KHJ/Scripts/BossDragon.cs
@@ -6,14 +6,15 @@ public class BossDragon : MonoBehaviour, IDamageable
 {
     public enum BossAttackType
     {
-        Land1, Land2, Land3, Land4, LandPattern1, LandPattern2, FlyPattern1, FlyPattern2, FlyPattern3, FlyPattern4, SIZE
+        Land1, Land2, Land3, Land4, LandPattern1, LandPattern2, Fly1, Fly2, FlyPattern1, FlyPattern2, SIZE
     }
 
     [SerializeField] Transform target;
     [SerializeField] BossCheck behindCheck;
     [SerializeField] BossAttack[] attacks;
     [SerializeField] int[] attackRates;
-    [SerializeField] int attackDelay;
+    [SerializeField] int landAttackDelay;
+    [SerializeField] int flyAttackDelay;
     [SerializeField] int attackCount;
 
     [SerializeField] bool isFlying = false;
@@ -42,7 +43,7 @@ public class BossDragon : MonoBehaviour, IDamageable
     Coroutine LandAttackCoroutine;
     IEnumerator LandAttackRoutine()
     {
-        WaitForSeconds delay = new WaitForSeconds(attackDelay);
+        WaitForSeconds delay = new WaitForSeconds(landAttackDelay);
 
         while (!isFlying)
         {
@@ -75,15 +76,50 @@ public class BossDragon : MonoBehaviour, IDamageable
             {
                 BossAttackType type = NextAttackType(BossAttackType.Land1, BossAttackType.Land4);
 
-                // Attack
+                // Move & Attack
+                StartCoroutine(MoveRoutine());
                 StartCoroutine(AttackRoutine(attacks[(int)type]));
             }
         }
     }
 
+    IEnumerator MoveRoutine()
+    {
+        float rotationSpeed = 5f;
+        float weight = 0.7f;
+
+        while (Vector3.Distance(transform.position, target.position) > 5f)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(target.position - transform.position), rotationSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, Vector3.Lerp(transform.position, target.position, weight * Time.deltaTime), moveSpeed);
+            yield return null;
+        }
+    }
+
+    Coroutine FlyAttackCoroutine;
+    IEnumerator FlyAttackRoutine()
+    {
+        WaitForSeconds delay = new WaitForSeconds(flyAttackDelay);
+
+        while (isFlying)
+        {
+            FlyAttack();
+            yield return delay;
+        }
+    }
+
     private void FlyAttack()
     {
+        // Pattern Attack
+        if (++attackCount % PatternAttackPeriod == 0)
+        {
 
+        }
+        // Attack
+        else
+        {
+
+        }
     }
 
     private BossAttackType NextAttackType(BossAttackType startType, BossAttackType endType)
@@ -108,6 +144,7 @@ public class BossDragon : MonoBehaviour, IDamageable
 
     IEnumerator AttackRoutine(BossAttack bossAttack)
     {
+        Debug.Log($"[Dragon] {bossAttack.gameObject.name}!");
         bossAttack.gameObject.SetActive(true);
         yield return new WaitForSeconds(attackJudgeTime);
         bossAttack.gameObject.SetActive(false);
@@ -125,6 +162,7 @@ public class BossDragon : MonoBehaviour, IDamageable
     private void Fly()
     {
         isFlying = true;
+        attackCount = 0;
         if (LandAttackCoroutine != null)
         {
             StopCoroutine(LandAttackCoroutine);
