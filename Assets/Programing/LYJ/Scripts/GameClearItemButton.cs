4c70ecdf341900949795cb13d270e4d04422dfb3
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-11-03 15:33:27 +0900

    [Feat]: 스테이지 클리어 UI 마우스 트리거 텍스트 활성화

--- Assets/Programing/LYJ/Scripts/GameClearItemButton.cs
+++ Assets/Programing/LYJ/Scripts/GameClearItemButton.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class GameClearItemButton : MonoBehaviour
 {
     public ItemData itemData;
-    [SerializeField] InGameController inGameController;
-    [SerializeField] Button button; //사진 버튼 (설명창 활성화)
-    [SerializeField] Button itemBuyButton;
+    [SerializeField] private InGameController inGameController;
+    [SerializeField] private Button button; //사진 버튼 (설명창 활성화)
+    [SerializeField] private Button itemBuyButton;
+    [SerializeField] private TextMeshProUGUI buttonText;
+    [SerializeField] private TextMeshProUGUI itemBuyButtonText;
 
     private bool isPurchased = false;
 
@@ -31,8 +33,10 @@ public class GameClearItemButton : MonoBehaviour
         inGameController = FindObjectOfType<InGameController>();
         button = GetComponent<Button>();
         itemBuyButton = transform.Find("Item Buy Button")?.GetComponent<Button>();
+        buttonText = transform.Find("Button Text")?.GetComponent<TextMeshProUGUI>();
+        itemBuyButtonText = transform.Find("Item Buy Button Text")?.GetComponent<TextMeshProUGUI>();
 
-        if (button != null )
+        if (button != null)
         {
             button.onClick.AddListener(OnButtonClick);
         }
@@ -52,7 +56,6 @@ public class GameClearItemButton : MonoBehaviour
             int randomIndex = Random.Range(0, CSVDownload.Instance.itemDataList.Count);
             itemData = CSVDownload.Instance.itemDataList[randomIndex];
 
-            // 아이템 데이터에 맞게 UI 업데이트
             button.GetComponentInChildren<TextMeshProUGUI>().text = itemData.itemName;
             itemBuyButton.GetComponent<Image>().sprite = itemData.itemImage;
         }
@@ -100,5 +103,43 @@ public class GameClearItemButton : MonoBehaviour
                 button.itemBuyButton.interactable = false;
             }
         }
+    }
+
+    private void AddEventTrigger(GameObject target, UnityEngine.Events.UnityAction<BaseEventData> enterAction, UnityEngine.Events.UnityAction<BaseEventData> exitAction)
+    {
+        EventTrigger trigger = target.GetComponent<EventTrigger>() ?? target.AddComponent<EventTrigger>();
+        EventTrigger.Entry enterEntry = new EventTrigger.Entry { eventID = EventTriggerType.PointerEnter };
+        enterEntry.callback.AddListener(enterAction);
+        trigger.triggers.Add(enterEntry);
+
+        EventTrigger.Entry exitEntry = new EventTrigger.Entry { eventID = EventTriggerType.PointerExit };
+        exitEntry.callback.AddListener(exitAction);
+        trigger.triggers.Add(exitEntry);
+    }
+
+    public void OnButtonHoverEnter(BaseEventData data)
+    {
+        buttonText.text = "상세보기";
+        buttonText.gameObject.SetActive(true);
+
+        itemBuyButtonText.gameObject.SetActive(false);
+    }
+
+    public void OnButtonHoverExit(BaseEventData data)
+    {
+        buttonText.gameObject.SetActive(false);
+    }
+
+    public void OnItemBuyButtonHoverEnter(BaseEventData data)
+    {
+        itemBuyButtonText.text = "선택하기";
+        itemBuyButtonText.gameObject.SetActive(true);
+
+        buttonText.gameObject.SetActive(false);
+    }
+
+    public void OnItemBuyButtonHoverExit(BaseEventData data)
+    {
+        itemBuyButtonText.gameObject.SetActive(false);
     }
 }
