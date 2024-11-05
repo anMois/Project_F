f40c5948a12e76c95008bfb530142f7a36f197b9
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-11-05 20:02:39 +0900

    [Feat]: 플레이어 스테이터스 변경 (공격력, 공격속도, 이동속도)

--- Assets/Programing/MKH/Script/Player.cs
+++ Assets/Programing/MKH/Script/Player.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Events;
+using static AttackBullet;
 
 public class Player : MonoBehaviour, IDamageable
 {
@@ -39,7 +40,7 @@ public class Player : MonoBehaviour, IDamageable
 
     private void Start()
     {
-        StatusWindowController.ItemChanged += Test;
+        StatusWindowController.ItemChanged += PlayerStatusChange;
     }
 
     private void Update()
@@ -101,111 +102,131 @@ public class Player : MonoBehaviour, IDamageable
         attack.speed = attackSpeed;
     }
 
+    /// <summary>
+    /// 공격력 증가
+    /// </summary>
+    /// <param name="value"></param>
     public void IncreaseDamage(int value)
     {
         Debug.Log("공격력 증가");
         dmg += value;
     }
 
-    private void Test(string itemName)
+    /// <summary>
+    /// 공격속도 증가
+    /// </summary>
+    /// <param name="value"></param>
+    public void IncreaseAttackSpeed(float percent)
+    {
+        Debug.Log("공격속도 증가");
+        attackSpeed += attackSpeed * (percent / 100);
+    }
+
+    /// <summary>
+    /// 이동속도 증가
+    /// </summary>
+    /// <param name="value"></param>
+    public void IncreaseMoveSpeed(float percent)
+    {
+        Debug.Log("이동속도 증가");
+        moveSpeed += moveSpeed * (percent / 100);
+    }
+
+    /// <summary>
+    /// 아이템에 따른 스탯을 변경하는 메서드
+    /// </summary>
+    /// <param name="itemName">아이템 이름</param>
+    /// <param name="atkIncrease">공격력</param>
+    /// <param name="atkSpeedIncreasePercent">공격속도</param>
+    /// <param name="moveSpeedIncreasePercent">이동속도</param>
+    private void ChangeStats(string itemName, int atkIncrease, float atkSpeedIncreasePercent, float moveSpeedIncreasePercent)
+    {
+        //UI 스테이터스 업데이트
+        StatusWindowController.Instance.ChangeStat("ATK", atkIncrease);
+        StatusWindowController.Instance.ChangeStat("ATS", (int)atkSpeedIncreasePercent);
+        StatusWindowController.Instance.ChangeStat("SPD", (int)moveSpeedIncreasePercent);
+
+        //실제 스테이터스 증가
+        IncreaseDamage(atkIncrease);
+        IncreaseAttackSpeed(atkSpeedIncreasePercent);
+        IncreaseMoveSpeed(moveSpeedIncreasePercent);
+    }
+
+    private void PlayerStatusChange(string itemName)
     {
         switch (itemName)
         {
             case "불같은 분노":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "오우거의 심장":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "증폭의 고서":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "파괴의 룬":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "빗발치는 불꽃":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "한줄기 태양":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "차가운 시선":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "날선 추위":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "몰아치는 눈폭풍":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "한파의 고서":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "얼어붙는 동파":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "얼음거울":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "천둥의 룬":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "전기 전도":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "무거운 벼락":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "플라즈마 방전":
-                IncreaseDamage(5);
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "치명의 고서":
-                IncreaseDamage(5);
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "올곶은 흐름":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "대지의 룬":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "가이아의 축복":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "굳은 심지":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "유연한 사고":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "철의 마음":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
             case "지진의 고서":
-                StatusWindowController.Instance.ChangeStat("ATK", 5);
-                IncreaseDamage(5);
+                ChangeStats(itemName, 10, 15, 5);
                 break;
         }
     }
